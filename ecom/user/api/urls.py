5031e05d969fe5fb2c48ff5bff6c25b0833cb0d7
agl5702 <advasquez41@misena.edu.co>
2024-12-28 17:25:20 -0500

    delete template and static folder in app store

--- ecom/user/api/urls.py
+++ ecom/user/api/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('user/', user_api_view, name='usuario_api'),
     path('user/<int:pk>/',user_detail_api_view, name='user_detail_api_view'),
     path('verify-user/',get_user_info_by_email, name='verify_user_email'),
-]
+] 
